64f44e52d9787b3824331101805f0a02acfd52c4
Tincho3604 <tincho3604@gmail.com>
2021-10-03 23:38:51 -0300

    Feat: Create Menú and Footer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+
+sections: {
+    politics: [
+        {id: 1, 
+        question: "¿Cual es el estadio mas grande de argentina?",
+        answer: "Boca Juniors",
+        section: "politics"
+        }
+    ]
+}
