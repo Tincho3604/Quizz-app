fd2af1fa06abe9572607514b36217744541ae82f
Tincho3604 <tincho3604@gmail.com>
2021-10-09 14:38:35 -0300

    Fix: Forms interactions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 // CONSTANTS
-const difficulty_thematic = document.getElementsByClassName("difficulty-thematic-main-container")
-const t = document.getElementById("test").style.display;
-
-//Functions
-const getFormDate = () => {
-    let arr = []
-    arr.push(document.getElementById("dificulty").value)
-    arr.push(document.getElementById("thematic").value)
-    return arr;
-} 
-
-console.log(getFormDate())
+const difficulty_thematic = document.getElementsByClassName("difficulty-thematic-main-container");
+const button_send_form1 = document.getElementById('id-submit-button-form1');
+const main_form = document.getElementById('id-main-form-header');
+const secondary_form = document.getElementById('id-difficulty-thematic-main-container')
 
 
-
+button_send_form1.addEventListener('click', (e) => {
+    e.preventDefault();
+    main_form.style.display = 'flex'
+    secondary_form.style.display= 'none'
+})
 
 
