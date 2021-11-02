45787d096c8f8d222c81c0e188405c19c2cc805b
Tincho3604 <tincho3604@gmail.com>
2021-11-02 17:42:56 -0300

    Feat: Add validation to first form and score

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // CONSTANTS
-const difficulty_thematic = document.getElementsByClassName("difficulty-thematic-main-container");
+const difficulty_thematic = document.getElementById('thematic');
 const button_send_form1 = document.getElementById('id-submit-button-form1');
 const main_form = document.getElementById('id-main-form-header');
 const secondary_form = document.getElementById('id-difficulty-thematic-main-container');
@@ -9,10 +9,19 @@ const form_message = document.getElementById('success-form-message');
 const openModal = document.querySelector('.hero__cta');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close');
+const correctQuestion = document.getElementById('correct-question');
+const wrongQuestion = document.getElementById('wrong-question');
+const scoreContainer = document.getElementById('score-span');
+
+//VARIABLES
+localStorage.setItem('thematic', 'Deporte');
+localStorage.setItem('correctAnswer', '0') 
+localStorage.setItem('wrongAnswers', '0')
+localStorage.setItem('score', '0')
 
 
 // RENDER MODAL
-openModal.addEventListener('click', (e)=>{
+openModal.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.add('modal--show');
 });
@@ -26,13 +35,12 @@ localStorage.setItem('IdCount', questionCount);
 form_message.style.display = 'none'
 
 
-//FUNCTIONS
 
 const searchCorrectAnswers = (arrNodes, selectValue) => {
   let divValue;
 
   for (let i = 0; i < arrNodes.length; i++) {
-       if(arrNodes[i].innerHTML === selectValue){
+      if(arrNodes[i].innerHTML === selectValue){
         index = arrNodes.indexOf(arrNodes[i]);
         divValue = arrNodes[i]
       }
@@ -46,16 +54,25 @@ const searchCorrectAnswers = (arrNodes, selectValue) => {
 }
 
 
+
+// Cambia de modo "Multiple Choise" a "Respuesta directa"
 const changeModeInput = () => {
   document.getElementById('form-answer').style.display = "block"
   document.getElementById('id-main-question-container').style.display = "none"
 } 
 
 
-const selectAnswer = (divElement, correctAnswer, IdCount) => {
 
+// Evalua si la pregunta fue bien constestada o no, en la opccion de multiple choise
+const selectAnswer = (divElement, correctAnswer, IdCount) => {
+  let scoreWins = localStorage.getItem('correctAnswer');
+  let scoreLoses = localStorage.getItem('wrongAnswers');
+  let score = localStorage.getItem('score');
+  
   const nodes = [...divElement.parentElement.children]
+  
   document.getElementById('change-options-button').style.display = "none"
+
   if(divElement.innerHTML === correctAnswer) {
 
     for(let i=0; i<nodes.length; i++) {
@@ -64,6 +81,10 @@ const selectAnswer = (divElement, correctAnswer, IdCount) => {
       nodes[i].classList.add("bad-answer-class");
       nodes[i].classList.remove("question-container");
     }
+
+    localStorage.setItem('correctAnswer', parseInt(scoreWins,10) + 1);
+    localStorage.setItem('score', parseInt(score,10) + 2);
+
     setTimeout(function() { 
       Swal.fire({
         title: '¡Respuesta correcta!',
@@ -76,7 +97,7 @@ const selectAnswer = (divElement, correctAnswer, IdCount) => {
     , 1500);
 
   } else {
-
+      localStorage.setItem('wrongAnswers', parseInt(scoreLoses,10) + 1);
       searchCorrectAnswers(nodes, correctAnswer)
       setTimeout(function() { 
         Swal.fire({
@@ -92,9 +113,15 @@ const selectAnswer = (divElement, correctAnswer, IdCount) => {
   
 }
 
+
+
+
+
+// Resetea el cuestionario
 const restartQuizz = () => location.reload()
 
 
+// Esta funcion cambia el Id de la pregunta
 const answerQuestion = () => {
   main_form.innerHTML = ''
   localStorage.setItem('IdCount', parseInt(localStorage.getItem('IdCount'), 10) + 1)
@@ -102,29 +129,40 @@ const answerQuestion = () => {
 }
 
 
-//VARIABLES
-let showQuestions = true;
+// Selecciona del JSON 5 preguntas y les asigna el ID del 1 al 5
+const selectRandomQuestions = (ar) => {
+  let num = Math.floor(Math.random() * (45 - 1 + 1) + 1)
+  let cont = 1
+  for(let i=0; i<5; i++){
+    ar.slice(num,num+5)[i].id = cont++;
+  }
+  return ar.slice(num,num+5)
+}
+
 
 //EVENTS
 button_send_form1.addEventListener('click', (e) => {
     e.preventDefault();
+    localStorage.setItem('thematic', difficulty_thematic.value)
     main_form.style.display = 'flex';
     secondary_form.style.display= 'none';
-    showQuestions = false;
+    renderForm(questionCount);
   })
 
 
 
+
+
+
+// Funcion que ejecuta el renderizado de las preguntas.
 const renderForm = (IdCount) => {
-  
-  fetch('questions.json')
+  fetch(`${localStorage.getItem('thematic')}.json`)
   .then(res => res.json())
   .then(data => QUESTIONS = data)
   .then(() => {
 
   if(IdCount < 6) { 
- 
-    QUESTIONS.filter(item => item.id === IdCount).map((question, index) => {
+    selectRandomQuestions(QUESTIONS).filter(item => item.id === IdCount).map(question => {
     localStorage.setItem('answer', question.correct);
     main_form.innerHTML += `
       <div class="main-answer-container">
@@ -155,7 +193,7 @@ const renderForm = (IdCount) => {
       document.getElementById('form-answer').style.display = "none"
       document.getElementById('id-main-question-container').style.display = "flex"
     })
-    })
+  })
 
 
 
@@ -163,17 +201,29 @@ const renderForm = (IdCount) => {
 
 // Chequea la respuesta correcta del input y suma 5 puntos y 0 si es incorrecta
 document.getElementById('form-answer').onsubmit = function (e) {
+  let scoreWins = localStorage.getItem('correctAnswer');
+  let scoreLoses = localStorage.getItem('wrongAnswers');
+  let score = localStorage.getItem('score');
+
+
   e.preventDefault();
   if (document.getElementById('form-answer').answer.value === localStorage.getItem('answer').toUpperCase()) {
     
+    localStorage.setItem('score', parseInt(score,10) + 5);
+    localStorage.setItem('correctAnswer', parseInt(scoreWins,10) + 1);
     Swal.fire({
       title: '¡Respuesta correcta!',
       text: `La respuesta correcta era ${localStorage.getItem('answer')}. Has ganado 5 puntos`,
       icon: 'success',
       confirmButtonText: `${ IdCount === 5 ? 'Ver resultados' : 'Siguiente pregunta'}`
     })
+
     answerQuestion();
+
   } else {
+
+    localStorage.setItem('wrongAnswers', parseInt(scoreLoses,10) + 1);
+    
     Swal.fire({
       title: '¡Respuesta incorrecta!',
       text: `La respuesta correcta era ${localStorage.getItem('answer')}. Has ganado 0 puntos`,
@@ -183,22 +233,18 @@ document.getElementById('form-answer').onsubmit = function (e) {
     answerQuestion();
   }
   document.getElementById('form-answer').reset();
-}
+}//-----------------------------------------------------------------------------------------------------------------------------
   } else {
-    main_form.style.display = 'none';
-    form_message.style.display = 'block'
-  }
-})
+
+      correctQuestion.innerHTML += localStorage.getItem('correctAnswer');
+      wrongQuestion.innerHTML += localStorage.getItem('wrongAnswers');
+      scoreContainer.innerHTML += localStorage.getItem('score');
+
+      main_form.style.display = 'none';
+      form_message.style.display = 'block';
+    }
+  })
 }
-
-
-
-if (showQuestions) {
-  renderForm(questionCount);
-}
-
-
-
 
 
 
